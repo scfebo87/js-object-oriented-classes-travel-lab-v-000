5205cebb8bb8e9d6b6ac683144d83c259598b446
Sean Christopher Febo <scf@techie.com>
2018-07-18 02:41:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ class Route {
     this.endingLocation = endingLocation; 
   }
   blocksTravelled() { 
-    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    const streetDistance = 
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    const streetDistance = (this.endingLocation.vertical - this.beginningLocation.vertical);
+    const avenueDistance = ()
 }
 
 
